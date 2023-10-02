f4bc267be706d8f07d79c7d653f99c191450feed
TuanLinh <ngotuanlinhvv@gmail.com>
2023-10-02 10:35:59 +0700

    linhLam

---
+++ src/duannhom/SinhVien.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duannhom;
+
+/**
+ *
+ * @author Ngo Nhan
+ */
+public class SinhVien {
+    private String maSV;
+    private String tenSV;
+    private int tuoi;
+
+    public SinhVien() {
+    }
+
+    public SinhVien(String maSV, String tenSV, int tuoi) {
+        this.maSV = maSV;
+        this.tenSV = tenSV;
+        this.tuoi = tuoi;
+    }
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public String getTenSV() {
+        return tenSV;
+    }
+
+    public void setTenSV(String tenSV) {
+        this.tenSV = tenSV;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    @Override
+    public String toString() {
+        return "SinhVien{" + "maSV=" + maSV + ", tenSV=" + tenSV + ", tuoi=" + tuoi + '}';
+    }
+    
+    
+}
