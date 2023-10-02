7bc269d7f09768d899d4f3b4deaef4ea59bc52cd
zzTRIzz <trinqph45719@fpt.edu.vn>
2023-10-02 10:04:41 +0700

    kk

--- src/xuong/Xuong.java
+++ src/xuong/Xuong.java
@@ -15,6 +15,8 @@ public class Xuong {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Chao");
+        
     }
     
 }
