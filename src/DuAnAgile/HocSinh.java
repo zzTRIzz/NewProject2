2ebeb05e14394aa96054a71bde6b9ba2cabc8798
TuanLinh <ngotuanlinhvv@gmail.com>
2023-11-08 22:46:15 +0700

    test

---
+++ src/DuAnAgile/HocSinh.java
@@ -0,0 +1,35 @@
+package DuAnAgile;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ngo Nhan
+ */
+public class HocSinh {
+    private String maHS;
+    private String tenHS;
+    private String gioiTinh;
+    private int namSinh;
+    private int diemToan;
+    private int diemVan;
+    private int diemAnh;
+
+    public HocSinh() {
+    }
+
+    public HocSinh(String maHS, String tenHS, String gioiTinh, int namSinh, int diemToan, int diemVan, int diemAnh) {
+        this.maHS = maHS;
+        this.tenHS = tenHS;
+        this.gioiTinh = gioiTinh;
+        this.namSinh = namSinh;
+        this.diemToan = diemToan;
+        this.diemVan = diemVan;
+        this.diemAnh = diemAnh;
+    }
+    
+    
+}
